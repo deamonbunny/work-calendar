60803c07aec887ab2b75c75bf0e7112621007e1c
bunny <deamonbunny@gmail.com>
2022-07-05 22:38:21 -0700

    updated to proper cell number and started formating, added JS file

---
+++ Develop/script.js
@@ -0,0 +1,5 @@
+//write event handler for sumbit button to save form info
+//keep forms from wiping data on submission
+//write event handler for page load to generate past saved schedule from local memory
+//get cells to change color with time
+//
