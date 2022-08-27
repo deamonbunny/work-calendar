e00cc98d176ea6453da8fc838ed8fc2ae95db51a
bunny <deamonbunny@gmail.com>
2022-08-26 21:06:50 -0700

    added time clock

--- Develop/script.js
+++ Develop/script.js
@@ -3,3 +3,11 @@
 //write event handler for page load to generate past saved schedule from local memory
 //get cells to change color with time
 //
+
+var timeFrame = setInterval(function(){
+    var currentTime = moment().format('MMMM Do YYYY, hh:mm:ss');
+    $("#currentDay").text(currentTime);
+}, 1000 );
+
+
+
